f562f8ad176da0a618b23db17312cd8041c3abc3
LaurenDarrimon <laurendarrimon@gmail.com>
2022-12-15 20:11:11 -0800

    update getstatic paths and get statci props to be async await, change data names on post page

--- pages/[id].js
+++ pages/[id].js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 
 //question for later - is "lib" naming Next.js convention or optional? 
-import { getPeopleIds, getData, getFriendList } from '../lib/data.js'
+import { getPeopleIds, getData } from '../lib/data.js'
 
 
 //create getstaticprops to return all the data for one person
@@ -12,14 +12,11 @@ import { getPeopleIds, getData, getFriendList } from '../lib/data.js'
 export async function getStaticProps({ params }) {
 
     const personData = await getData(params.id);
-    const friendData = await getFriendList(params.id)
 
-    console.log("friend data on id page")
-    console.log(friendData)
 
     return {
         props: {
-            personData, friendData
+            personData
         }
     };
 }
@@ -27,7 +24,7 @@ export async function getStaticProps({ params }) {
 //all possible URLs 
 export async function getStaticPaths() {
 
-    const dynamicPaths = getPeopleIds();
+    const dynamicPaths = await getPeopleIds();
 
     return {
         paths: dynamicPaths,
@@ -37,36 +34,22 @@ export async function getStaticPaths() {
 
 //make a compoenent that will display the persons details at the dynamic route 
 
-export default function Entry (  { personData , friendData } ){
+export default function Entry (  { personData } ){
     return (
         <Layout>
             <article className="card col-6">
                 <div className="card-body">
                     <h4 className="card-title text-dark">
-                        {personData.name}
+                        {personData.post_title}
                     </h4>
                     <h5 className="card-subtitle mb-2 text-muted"> 
-                        {personData.job}
+                        published: {personData.post_date}
                     </h5>
-                    <p className="card-text text-dark">{personData.name} was {personData.age} years old at the time of the Fellowship of the Ring.</p>
-                    <a href={"mailto:" + personData.email} className="card-link text-primary"> Email {personData.name}</a>
+                    <p className="card-text text-dark">{personData.post_content}</p>
                     <br/>
                     <h5 className="card-text text-dark">
-                        Friends of {personData.name} :
-                    </h5>
-                    {console.log("friend data inside component")}
-                    {console.log(friendData)}
-                        {friendData &&
-                            <div className="list-group">
-                           
-
-                            {friendData.map(({ friendId, friendName }) => (
-                                <Link  key={friendId} href={`/${friendId}`}>
-                                    <a className="list-group-item text-dark list-group-item-action"> {friendName} </a> 
-                                </Link>
-                            ))}
-                            </div>
-                        }        
+                        Status: {personData.post_status}
+                    </h5>   
                 </div>
             </article>
         </Layout>
